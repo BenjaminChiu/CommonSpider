79c2212d9bb5a58bb6c1416286f18c651d09fbbb
Benjamin Chiu <topfisherman@126.com>
2021-05-07 21:52:13 +0800

    子线程修改

--- my_thread/ThreadOne.py
+++ my_thread/ThreadOne.py
@@ -4,24 +4,15 @@ import random
 import threading
 import time
 
-import cfg
 import model.request_model as my_request
-from do_main.dytt import dytt_Lastest
 from model.request_model import new_request
-from model.task_queue import TaskQueue
 
 '''
-    1)自己封装抓取二级网页多线程
-    2)由一级链接 抓取 电影目录
-    例如：由 http://www.dytt8.net/html/gndy/dyzz/list_23_2.html 获取
-           "2017年动画喜剧《宝贝老板》英国粤三语.BD中英双字幕" 和 "页面 url 地址"等若干条电影的信息
-@Author monkey
-@Date 2017-08-08
+request queue1，put response to Queue2
 '''
 
 
 class ThreadOne(threading.Thread):
-    NOT_EXIST = 0
 
     def __init__(self, id, queue):
         threading.Thread.__init__(self)
@@ -39,8 +30,8 @@ class ThreadOne(threading.Thread):
                 response.encoding = 'GBK'
 
                 if response.status_code != 200:  # 请求失败
-                    self.queue.put(url)  # 将URL 重新加入队列，并休眠20ms
-                    time.sleep(20)
+                    self.queue.put(url)  # 将URL重新加入队列，并休眠20ms
+                    return None
                 else:  # 请求成功
                     # 获取一页上所有的 电影的链接，并存入容器，返回一个List
                     # moivePageUrlList = dytt_Lastest.getMoivePageUrlList(response.text)
@@ -49,7 +40,8 @@ class ThreadOne(threading.Thread):
                     #     # print("在FloorWorkThread中，每部具体电影的URL："+each)
                     #     TaskQueue.putToQueue_2(each)  # 将页面上许多电影的链接 存入queue2
                     return response.text
-                # 支线程 随机休眠
-                time.sleep(random.randint(5, 20))
             except Exception as e:
-                print('1代子线程出现问题：' + e)
+                print('1代子线程出现问题：' + str(e))
+                return None
+            finally:
+                time.sleep(random.randint(5, 20))  # 子线程 随机休眠
--- my_thread/ThreadTwo.py
+++ my_thread/ThreadTwo.py
@@ -4,39 +4,27 @@ import random
 import threading
 import time
 
-import requests
-
 from do_main.dytt import dytt_Lastest
-from model.request_model import RequestModel
+from model.request_model import new_request
 from model.task_queue import TaskQueue
 
 '''
-    1)从电影详细信息页面【http://www.dytt8.net/html/gndy/dyzz/20170806/54695.html】中抓取目标内容
-    2)将数据存储到数据库中
+request queue2，put response to Queue3
 '''
 
 
 class ThreadTwo(threading.Thread):
-    NOT_EXIST = 0
-
-    def __init__(self, queue, id):
+    def __init__(self, id, queue):
         threading.Thread.__init__(self)
-        self.queue = queue
         self.id = id
+        self.queue = queue
 
     def run(self):
         # 循环取出 电影链接队列，分析页面了
-        while not self.NOT_EXIST:
-            # 队列为空, 结束
-            if self.queue.empty():
-                NOT_EXIST = 1
-                self.queue.task_done()
-                break
-
+        while not self.queue.empty():
             url = self.queue.get()
-            requests_model = RequestModel()
             try:
-                response = requests_model.new_request(url)
+                response = new_request(url)
                 print('线程2代 子线程 ' + str(self.id) + ' 请求【 ' + url + ' 】的结果： ' + str(response.status_code))
                 response.close()  # 为什么加这个？？？？  原因：出现了 远程主机强迫关闭了一个现有的连接
 
@@ -45,7 +33,6 @@ class ThreadTwo(threading.Thread):
 
                 if response.status_code != 200:
                     self.queue.put(url)
-                    time.sleep(20)
                 else:
                     # 分析 页面，将内容加入队列。一个队列中的元素就是一部完整的电影
                     temp = dytt_Lastest.getMoiveInforms(response.text)
@@ -57,9 +44,7 @@ class ThreadTwo(threading.Thread):
                         # TaskQueue.getContentQueue().join()
                         print("当前队列数量=" + str(TaskQueue.getQueue_3().qsize()))
 
-                # 线程沉睡5秒
-                time.sleep(random.randint(5, 20))
-
             except Exception as e:
-                # self.queue.put(url)
-                print(e)
+                print('子线程2代出现问题' + str(e))
+            finally:
+                time.sleep(random.randint(5, 20))  # 线程沉睡5秒
