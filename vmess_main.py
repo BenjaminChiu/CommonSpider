8cae654a1a03d5053d983dd81a63783f922bb162
Benjamin Chiu <topfisherman@126.com>
2021-07-13 23:33:05 +0800

    全页面抓取数据、使用正则表达式过滤数据

--- vmess_main.py
+++ vmess_main.py
@@ -13,7 +13,7 @@ import cfg
 from model.request_model import MyRequest, MySession
 
 # 正则表达式匹配vmess
-pattern = re.compile(r'^(vmess://|ss://)\w')
+pattern = re.compile(r'^(vmess://|ss://|ssr://)\w')
 
 
 def init():
@@ -28,12 +28,13 @@ def init():
         response_2 = MyRequest(session, day[i]).my_get()
         print("监测点2：链接成功")
         selector_2 = etree.HTML(response_2.text)
-        data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[3]/div/text()")  # 注意脚标是1开始，而非0
-        # 做兼容
-        if len(data) == 0:
-            data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[4]/div/text()")
-        if len(data) == 0:
-            data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[5]/div/text()")
+        # 获取到了整个div的所有text，用于后面筛选
+        div_data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div//text()")
+
+        for j in range(len(div_data)):
+            if pattern.match(div_data[j]):
+                data.append(div_data[j])
+
         # 能拿到信息，即结束
         if len(data) != 0:
             print("今日链接为：%s" % day[i])
@@ -49,13 +50,8 @@ def init():
 
 
 if __name__ == '__main__':
-    # init()
+    init()
 
-    match = pattern.match(
-        'vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogImh0dHBzOi8vd3d3Lm1hdHRrYXlkaWFyeS5jb21844CQ55u06L+e44CRSEtHL+mmmea4ryBBIC0gOCIsDQogICJhZGQiOiA'
-        'iMDA4LkhLLkFQLlBPUC5CSUdBSVJQT1JULk5FVCIsDQogICJwb3J0IjogIjgwIiwNCiAgImlkIjogImJiNTExMzQ3LTZlZDUtNDc1Yy05ZmZlLWFjNzI2OGZkZjM1MSIsDQogICJ'
-        'haWQiOiAiMSIsDQogICJzY3kiOiAiYXV0byIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAiIiwNCiAgInBhdGgiOiAiIiwNCiAgInRscyI'
-        '6ICIiLA0KICAic25pIjogIiINCn0=')
-    match_2 = pattern.match('ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaRUNTbGFUWmVhNWpGTzczNkZBM1J5M0RhWFJBMnFOeDBwY2VZRHpLQ3VZUzhnbDNCNHA4SW53eURDT1N4OUAxNT'
-                            'QuMTcuMi4yMTE6MTgzMzI=#https%3a%2f%2fwww.mattkaydiary.com%7c%e7%be%8e%e5%9b%bd%e3%8a%b4%e7%9b%b4%e8%bf%9e')
-    print("%s" % match_2.string)
+    # match_2 = pattern.match('ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaRUNTbGFUWmVhNWpGTzczNkZBM1J5M0RhWFJBMnFOeDBwY2VZRHpLQ3VZUzhnbDNCNHA4SW53eURDT1N4OUAxNT'
+    #                         'QuMTcuMi4yMTE6MTgzMzI=#https%3a%2f%2fwww.mattkaydiary.com%7c%e7%be%8e%e5%9b%bd%e3%8a%b4%e7%9b%b4%e8%bf%9e')
+    # print("%s" % match_2.string)
