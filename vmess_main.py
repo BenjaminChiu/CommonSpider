42ac51dddd9e4be74535002c947c40b666b0cd8a
Benjamin Chiu <topfisherman@126.com>
2021-07-13 15:51:56 +0800

    已经实现添加vmess://正则表达式，待添加全页面抓取。git test.

--- vmess_main.py
+++ vmess_main.py
@@ -5,11 +5,15 @@
 @File   : vmess_main.py
 @coding : utf-8
 """
+import re
 
 from lxml import etree
 
 import cfg
 from model.request_model import MyRequest, MySession
+
+# 正则表达式匹配vmess
+pattern = re.compile(r'^(vmess://|ss://)\w')
 
 
 def init():
@@ -24,10 +28,10 @@ def init():
         response_2 = MyRequest(session, day[i]).my_get()
         print("监测点2：链接成功")
         selector_2 = etree.HTML(response_2.text)
-        data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[4]/div/text()")  # 注意脚标是1开始，而非0
+        data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[3]/div/text()")  # 注意脚标是1开始，而非0
         # 做兼容
         if len(data) == 0:
-            data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[3]/div/text()")
+            data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[4]/div/text()")
         if len(data) == 0:
             data = selector_2.xpath("//div[@style='-webkit-text-stroke-width: 0px;']/div[5]/div/text()")
         # 能拿到信息，即结束
@@ -45,4 +49,13 @@ def init():
 
 
 if __name__ == '__main__':
-    init()
+    # init()
+
+    match = pattern.match(
+        'vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogImh0dHBzOi8vd3d3Lm1hdHRrYXlkaWFyeS5jb21844CQ55u06L+e44CRSEtHL+mmmea4ryBBIC0gOCIsDQogICJhZGQiOiA'
+        'iMDA4LkhLLkFQLlBPUC5CSUdBSVJQT1JULk5FVCIsDQogICJwb3J0IjogIjgwIiwNCiAgImlkIjogImJiNTExMzQ3LTZlZDUtNDc1Yy05ZmZlLWFjNzI2OGZkZjM1MSIsDQogICJ'
+        'haWQiOiAiMSIsDQogICJzY3kiOiAiYXV0byIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAiIiwNCiAgInBhdGgiOiAiIiwNCiAgInRscyI'
+        '6ICIiLA0KICAic25pIjogIiINCn0=')
+    match_2 = pattern.match('ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaRUNTbGFUWmVhNWpGTzczNkZBM1J5M0RhWFJBMnFOeDBwY2VZRHpLQ3VZUzhnbDNCNHA4SW53eURDT1N4OUAxNT'
+                            'QuMTcuMi4yMTE6MTgzMzI=#https%3a%2f%2fwww.mattkaydiary.com%7c%e7%be%8e%e5%9b%bd%e3%8a%b4%e7%9b%b4%e8%bf%9e')
+    print("%s" % match_2.string)
