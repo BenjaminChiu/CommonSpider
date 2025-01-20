a564425c98a5c8e2b8112ee953eb2c73524ad7e7
Donkey <topfisherman@126.com>
2025-01-20 11:47:52 +0800

    年前备份提交

--- Test.plugin/popup.js
+++ Test.plugin/popup.js
@@ -1,10 +1,10 @@
 document.getElementById('clickMe').addEventListener('click', () =>
 {
-    let pdf = document.getElementsByTagName('embed')[0];
-    pdf.focus();
-
-
-    navigator.clipboard.writeText('fuck me_1');
+    // let pdf = document.getElementsByTagName('embed')[0];
+    // pdf.focus();
+    //
+    //
+    // navigator.clipboard.writeText('fuck me_1');
 
     alert('测试弹窗');
     console.log('点击函数完成');
